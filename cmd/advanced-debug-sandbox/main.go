4b12b7c036d23865931c7354f7772eb48afcbfd6
Antoine Beauchamp <end2endzone@gmail.com>
2026-09-06 15:46:20 -0400

    Greatly redesigned the cmd\advanced-debug-sandbox project. Now creating much more snapshots before "now" to allow proper debugging of command: `fldrecall --recall`.

--- cmd/advanced-debug-sandbox/main.go
+++ cmd/advanced-debug-sandbox/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"time"
 
 	"github.com/end2endzone/FolderRecall/internal/recall"
 	"github.com/end2endzone/FolderRecall/internal/utils"
@@ -56,17 +57,54 @@ func FindProjectDir() (string, error) {
 	return "", fmt.Errorf("can not find project directory")
 }
 
-func GetTempDirectoryPath(sandboxDir string, index int) string {
-	tempDir := filepath.Join(sandboxDir, "temp")
+func FindDebugSandboxDir() string {
+	// Find the project's root directory
+	projectDir, err := FindProjectDir()
+	if err != nil {
+		panic(err)
+	}
+
+	// Find sandbox directory
+	sandboxDir := filepath.Join(projectDir, ".debug-sandbox")
+	err = utils.DirExistsOrError(sandboxDir)
+	if err != nil {
+		panic(err)
+	}
+
+	return sandboxDir
+}
+
+func FindRecallDir() string {
+	sandboxDir := FindDebugSandboxDir()
+
+	testDataDir := filepath.Join(sandboxDir, "testdata")
+	err := utils.DirExistsOrError(testDataDir)
+	if err != nil {
+		panic(err)
+	}
+
+	recallDir := filepath.Join(testDataDir, "recall")
+	err = os.MkdirAll(recallDir, 0o755)
+	if err != nil {
+		panic(err)
+	}
+
+	return recallDir
+}
+
+func GetRecallTempDirectoryFromIndex(recallDir string, index int) string {
+	subDirs := filepath.Join(recallDir, "subdirs")
 	name := fmt.Sprintf("%03d", index)
-	subDirPath := filepath.Join(tempDir, name)
+	subDirPath := filepath.Join(subDirs, name)
 	return subDirPath
 }
 
-func CreateTempDirectories(sandboxDir string) error {
+func CreateRecallTestDirectories() error {
+	recallDir := FindRecallDir()
+
 	// Create 20 subdirectories
 	for i := 0; i < 20; i++ {
-		subDirPath := GetTempDirectoryPath(sandboxDir, i)
+		subDirPath := GetRecallTempDirectoryFromIndex(recallDir, i)
 		err := os.MkdirAll(subDirPath, 0o755)
 		if err != nil {
 			return err
@@ -76,23 +114,78 @@ func CreateTempDirectories(sandboxDir string) error {
 	return nil
 }
 
-func CreateRecallTestDatabase(sandboxDir string) error {
-	testDataDir := filepath.Join(sandboxDir, "testdata")
-	err := utils.DirExistsOrError(testDataDir)
-	if err != nil {
-		return err
+func AddSingleMockSnapshot(snapshots *[]*recall.Snapshot, age time.Duration, path string) {
+	now := time.Now().Round(0)
+
+	snapshot := &recall.Snapshot{
+		Id:        recall.InvalidId,
+		Timestamp: now.Add(-age),
+		Directories: []recall.Directory{
+			{
+				Id:   recall.InvalidId,
+				Path: path,
+			},
+		},
 	}
 
-	recallDir := filepath.Join(testDataDir, "recall")
-	err = os.MkdirAll(recallDir, 0o755)
-	if err != nil {
-		return err
+	*snapshots = append(*snapshots, snapshot)
+}
+
+func CreateRecallMockSnapshots() ([]*recall.Snapshot, error) {
+	recallDir := FindRecallDir()
+
+	snapshots := []*recall.Snapshot{}
+
+	const oneDay = time.Duration(24 * time.Hour)
+	const oneWeek = time.Duration(7 * oneDay)
+
+	// Time offsets to simulate multiple days and different times of day
+	// (e.g., increments of several hours to spread across a 3-day window)
+	timeOffsets := []time.Duration{
+		3 * oneWeek,      // 3 weeks ago
+		2 * oneWeek,      // 2 weeks ago
+		8 * oneDay,       // 8 days ago
+		7 * oneDay,       // 7 days ago
+		6 * oneDay,       // 6 days ago
+		5 * oneDay,       // 5 days ago
+		4 * oneDay,       // 4 days ago
+		3 * oneDay,       // 3 days ago
+		2 * oneDay,       // 2 days ago
+		1 * oneDay,       // 1 days ago
+		6 * time.Hour,    // 6 hours ago
+		5 * time.Hour,    // 5 hours ago
+		4 * time.Hour,    // 4 hours ago
+		1 * time.Hour,    // 1 hours ago
+		55 * time.Minute, // 55 minutes ago
+		15 * time.Minute, // 15 minutes ago
+		10 * time.Minute, // 10 minutes ago
+		5 * time.Minute,  // 5 minutes ago
+		0 * time.Hour,    // now
 	}
+
+	for i, timeOffset := range timeOffsets {
+		path := GetRecallTempDirectoryFromIndex(recallDir, i)
+		AddSingleMockSnapshot(&snapshots, timeOffset, path)
+	}
+
+	return snapshots, nil
+}
+
+func CreateRecallTestDatabase() error {
+	recallDir := FindRecallDir()
 
 	dbFilePath := filepath.Join(recallDir, "recall.db")
 
+	// Delete the database if it alreadu exists
+	if utils.FileExists(dbFilePath) {
+		err := os.Remove(dbFilePath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	var dbConn *sql.DB
-	dbConn, err = sql.Open("sqlite", dbFilePath)
+	dbConn, err := sql.Open("sqlite", dbFilePath)
 	defer func() {
 		if dbConn != nil {
 			dbConn.Close()
@@ -114,11 +207,10 @@ func CreateRecallTestDatabase(sandboxDir string) error {
 	}
 
 	// Create mock snapshots
-	snapshot, err := recall.CreateSnapshotNow()
+	snapshots, err := CreateRecallMockSnapshots()
 	if err != nil {
 		return err
 	}
-	snapshots := []*recall.Snapshot{&snapshot}
 
 	// Insert into the database
 	err = recall.InsertSnapshots(dbConn, snapshots)
@@ -131,6 +223,12 @@ func CreateRecallTestDatabase(sandboxDir string) error {
 	dbConn = nil
 
 	fmt.Printf("Recall test database created: %s\n", dbFilePath)
+
+	// Print snapshot details
+	fmt.Printf("with the following snapshots:")
+	for _, snapshot := range snapshots {
+		fmt.Printf("%s\n", snapshot.String())
+	}
 
 	return nil
 }
@@ -152,13 +250,13 @@ func main() {
 	fmt.Printf("Building advanced debug sandbox in directory %s\n", sandboxDir)
 
 	// Create more directories to use as snapshots
-	err = CreateTempDirectories(sandboxDir)
+	err = CreateRecallTestDirectories()
 	if err != nil {
 		panic(err)
 	}
 
 	// Create fake database for recall
-	err = CreateRecallTestDatabase(sandboxDir)
+	err = CreateRecallTestDatabase()
 	if err != nil {
 		panic(err)
 	}
